4e53f3a4448ddf1be63207eaf73a9745fdcca0ce
Juan Monteiro Veronez <jmonteiroveronez@apple.com>
2023-11-07 08:15:22 -0300

    adds `compose.yml` to create database and `database.js` to connect with it, using `.env` to keep the enviroments and `.env.public` to show what is necessary.

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,3 +1,10 @@
-export default function status(request, response) {
+import database from "../../../../../infra/database.js";
+
+console.log(database);
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum");
+  console.log(result.rows);
+
   response.status(200).json({ message: "Hello" });
 }
