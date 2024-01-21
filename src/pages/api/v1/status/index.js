5fca2a6a95df49ca4aa67a1b955c42d6524c2d04
Juan Monteiro Veronez <jmonteiroveronez@apple.com>
2024-01-21 15:18:39 -0300

    finishes /status endpoint, returning a first status format

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,10 +1,34 @@
 import database from "infra/database.js";
 
-console.log(database);
-
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json({ message: "Hello" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW MAX_CONNECTIONS;",
+  );
+  const databaseMaxConnections =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnections =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        upened_connections: databaseOpenedConnections,
+      },
+    },
+  });
 }
--- src/tests/integration/api/v1/status/get.test.js
+++ src/tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,38 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+describe("GET to /api/v1/status", () => {
+  let response;
+  let responseBody;
+  beforeAll(async () => {
+    response = await fetch("http://localhost:3000/api/v1/status");
+    responseBody = await response.json();
+  }, 10000);
 
-  expect(response.status).toBe(200);
+  it("should return 200", async () => {
+    expect(response.status).toBe(200);
+  }, 10000);
+
+  it("should return correct updated_at", async () => {
+    expect(responseBody.updated_at).toBeDefined();
+
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+  }, 10000);
+
+  it("should return database version", () => {
+    const databaseVersion = responseBody.dependencies.database.version;
+
+    expect(databaseVersion).toBe("16.0");
+  }, 10000);
+
+  it("should return database max number of connections", () => {
+    const maxConnections = responseBody.dependencies.database.max_connections;
+
+    expect(maxConnections).toBe(100);
+  }, 10000);
+
+  it("should return database number of upened connections", () => {
+    const usedConnections =
+      responseBody.dependencies.database.upened_connections;
+
+    expect(usedConnections).toBe(1);
+  }, 10000);
 });
